de9425fd56bcbd1a3408b80474bf5ba820287de6
Denis Barinov <seocoup@yandex.ru>
2022-03-23 21:17:21 +0300

    Условные операторы - Задание 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int year = 400;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0  && year % 400 != 0) {
+                System.out.println("Год не високосный");
+            } else {
+                System.out.println("Год високосный");
+                }
+            }
+        else {
+            System.out.println("Год не високосный");
+             }
+
 
     }
 }
+
