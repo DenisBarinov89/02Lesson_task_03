05146f6f6db259d04b20a8bfc4935501cf39689d
PENETRON <49232320+DenisBarinov89@users.noreply.github.com>
2022-03-31 10:33:43 +0300

    Revert "Условные операторы - Задание 3"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int year = 2024;
 
-        if (year % 4 != 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println("Год не високосный");
-        } else {
-            System.out.println("Год високосный");
-        }
-        // }
-        // else {
-        // System.out.println("Год не високосный");
-        // }
     }
 }
-
